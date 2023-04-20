b28b21d82a8c4a271f6e8f4f363920f2412f17dd
dyn T <78135443+LIBai11@users.noreply.github.com>
2023-04-21 06:35:56 +0800

    更新 main.js

--- patch-demo-npm/src/main.js
+++ patch-demo-npm/src/main.js
@@ -1,4 +1,22 @@
-import {createApp} from 'vue'
-import App from './App.vue'
-import XPicker from "x-picker";
-createApp(App).use(XPicker).mount('#app')
+//import {createApp} from 'vue'
+//import App from './App.vue'
+//import XPicker from "x-picker";
+//createApp(App).use(XPicker).mount('#app')
+
+function modify(t) {
+  if (t.hasOwnProperty('data')) {
+    t.data.forEach((item, index) => {
+      if (item.code === 'NOT_START') {
+        console.log(item)
+        t.data[index].code = 'CAN_RECEIVE';
+        t.data[index].couponValidityStartTime = '1681956001000';
+      }
+    });
+  }
+  return t;
+}
+
+let body = $response.body;
+console.log(body)
+//body = JSON.stringify(modify(JSON.parse(body)));
+$done({ body: body });
